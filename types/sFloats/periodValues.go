ca1f63e9720b398e31ebf9981eb7f2dded346135
Yassel DG <yasseldg@gmail.com>
2024-03-01 10:36:34 +0000

    Updating sFloats

---
+++ types/sFloats/periodValues.go
@@ -0,0 +1,134 @@
+package sFloats
+
+import (
+	"sync"
+
+	"github.com/yasseldg/go-simple/logs/sLog"
+
+	"gonum.org/v1/gonum/stat"
+)
+
+// ---- PeriodValues
+
+type PeriodValues struct {
+	mu sync.Mutex
+
+	period int
+	values []float64
+
+	filled bool
+}
+
+func NewPeriodValues(period int) *PeriodValues {
+	return &PeriodValues{
+		mu:     sync.Mutex{},
+		period: period,
+		values: make([]float64, 0, period),
+	}
+}
+
+func (pv *PeriodValues) Values() []float64 {
+	pv.mu.Lock()
+	defer pv.mu.Unlock()
+
+	return pv.values
+}
+
+func (pv *PeriodValues) Filled() bool {
+	pv.mu.Lock()
+	defer pv.mu.Unlock()
+
+	return pv.filled
+}
+
+func (pv *PeriodValues) ToFill() int {
+	pv.mu.Lock()
+	defer pv.mu.Unlock()
+
+	return pv.period - len(pv.values)
+}
+
+func (pv *PeriodValues) Log(qty int) {
+	pv.mu.Lock()
+	defer pv.mu.Unlock()
+
+	if len(pv.values) < 1 {
+		sLog.Info("PeriodValues.Log: periods: %-5d  ..  len: %-5d", pv.period, len(pv.values))
+		return
+	}
+	if len(pv.values) < qty {
+		sLog.Info("PeriodValues.Log: periods: %-5d  ..  len: %-5d  ..  values: %v ", pv.period, len(pv.values), pv.values)
+		return
+	}
+	sLog.Info("PeriodValues.Log: periods: %-5d  ..  len: %-5d  ..  firsts: %v  ..  lasts: %v", pv.period, len(pv.values), pv.values[:3], pv.values[len(pv.values)-3:])
+}
+
+func (pv *PeriodValues) Add(value float64) {
+	pv.mu.Lock()
+	defer pv.mu.Unlock()
+
+	if !pv.filled {
+		pv.add(value)
+		return
+	}
+
+	pv.values = append(pv.values[1:], value)
+}
+
+func (pv *PeriodValues) add(value float64) {
+	pv.values = append(pv.values, value)
+
+	if len(pv.values) >= pv.period {
+		pv.filled = true
+	}
+}
+
+// Fill adds a value to the PeriodValues if it is not filled yet
+// and returns true. If the PeriodValues is already filled, it returns
+// false. Fill inverse order of Add.
+func (pv *PeriodValues) Fill(value float64) bool {
+	pv.mu.Lock()
+	defer pv.mu.Unlock()
+
+	return pv.fill(value)
+}
+
+func (pv *PeriodValues) fill(value float64) bool {
+	if pv.filled {
+		return false
+	}
+
+	if len(pv.values) >= pv.period {
+		pv.filled = true
+		return false
+	}
+
+	pv.values = append([]float64{value}, pv.values...)
+
+	return true
+}
+
+func (pv *PeriodValues) DeepCopy() *PeriodValues {
+	pv.mu.Lock()
+	defer pv.mu.Unlock()
+
+	return pv.deepCopy()
+}
+
+func (pv *PeriodValues) deepCopy() *PeriodValues {
+	newValues := make([]float64, len(pv.values))
+	copy(newValues, pv.values)
+
+	return &PeriodValues{
+		mu:     sync.Mutex{},
+		period: pv.period, // Los valores de tipo int se copian por valor
+		values: newValues,
+	}
+}
+
+func (pv *PeriodValues) Mean() float64 {
+	if len(pv.values) < pv.period {
+		return 0
+	}
+	return stat.Mean(pv.values, nil)
+}
---
+++ types/sFloats/periodValuesMinMax.go
@@ -0,0 +1,118 @@
+package sFloats
+
+import (
+	"fmt"
+
+	"github.com/yasseldg/go-simple/logs/sLog"
+)
+
+// ---- PeriodValues
+
+type PeriodValuesMinMax struct {
+	*PeriodValues
+
+	min_max bool
+	min     float64
+	max     float64
+}
+
+func NewPeriodValuesMinMax(period int, min_max bool) *PeriodValuesMinMax {
+	return &PeriodValuesMinMax{
+		PeriodValues: NewPeriodValues(period),
+		min_max:      min_max,
+	}
+}
+
+func (pv *PeriodValuesMinMax) Min() float64 {
+	pv.mu.Lock()
+	defer pv.mu.Unlock()
+
+	return pv.min
+}
+
+func (pv *PeriodValuesMinMax) Max() float64 {
+	pv.mu.Lock()
+	defer pv.mu.Unlock()
+
+	return pv.max
+}
+
+func (pv *PeriodValuesMinMax) Log(qty int) {
+	msg := fmt.Sprintf("PeriodValues.Log: periods: %-5d  ..  len: %-5d  ..  min: %f  ..  max: %f", pv.period, len(pv.values), pv.min, pv.max)
+	if len(pv.values) < 1 {
+		sLog.Warn(msg)
+		return
+	}
+	if len(pv.values) < qty {
+		sLog.Warn("%s  ..  values: %v ", msg, pv.values)
+		return
+	}
+	sLog.Warn("PeriodValues.Log: periods: %-5d  ..  len: %-5d  ..  firsts: %v  ..  lasts: %v", pv.period, len(pv.values), pv.values[:3], pv.values[len(pv.values)-3:])
+}
+
+func (pv *PeriodValuesMinMax) Add(value float64) {
+	pv.mu.Lock()
+	defer pv.mu.Unlock()
+
+	if !pv.filled {
+		pv.add(value)
+
+		if len(pv.values) == 1 {
+			pv.min = value
+			pv.max = value
+			return
+		}
+
+		pv.minMax(value)
+		return
+	}
+
+	del := pv.values[0]
+	pv.values = append(pv.values[1:], value)
+
+	if del == pv.min || del == pv.max {
+		pv.min, pv.max = MinMax(pv.values, false)
+		return
+	}
+
+	pv.minMax(value)
+}
+
+func (pv *PeriodValuesMinMax) minMax(value float64) {
+	if value > pv.max {
+		pv.max = value
+		return
+	}
+	if value < pv.min {
+		pv.min = value
+	}
+}
+
+func (pv *PeriodValuesMinMax) Fill(value float64) bool {
+	pv.mu.Lock()
+	defer pv.mu.Unlock()
+
+	r := pv.fill(value)
+
+	if len(pv.values) == 1 {
+		pv.min = value
+		pv.max = value
+		return r
+	}
+
+	pv.minMax(value)
+
+	return r
+}
+
+func (pv *PeriodValuesMinMax) DeepCopy() *PeriodValuesMinMax {
+	pv.mu.Lock()
+	defer pv.mu.Unlock()
+
+	return &PeriodValuesMinMax{
+		PeriodValues: pv.PeriodValues.deepCopy(),
+		min_max:      pv.min_max,
+		min:          pv.min,
+		max:          pv.max,
+	}
+}
