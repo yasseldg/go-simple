49fce56d3efe091249fc46246852f2cc7e797ea0
Yassel DG <yasseldg@gmail.com>
2024-03-13 18:23:10 +0000

    Updating sFloats and sJson

---
+++ types/sFloats/smoothedAvg.go
@@ -0,0 +1,94 @@
+package sFloats
+
+import "sync"
+
+type SmoothedAverage struct {
+	mu sync.Mutex
+
+	period int
+	value  float64
+
+	filled int
+}
+
+func NewSmoothedAverage(period int) *SmoothedAverage {
+	return &SmoothedAverage{
+		mu: sync.Mutex{},
+
+		period: period,
+		filled: period,
+	}
+}
+
+// AddPos adds a value to the average
+func (sa *SmoothedAverage) AddPos(v float64) {
+	sa.mu.Lock()
+	defer sa.mu.Unlock()
+
+	if sa.filled > 0 {
+		sa.fillPos(v)
+		return
+	}
+
+	sa.value *= float64(sa.period - 1)
+	sa.value += v
+	sa.value /= float64(sa.period)
+}
+
+// AddNeg subtracts a value from the average
+func (sa *SmoothedAverage) AddNeg(v float64) {
+	sa.mu.Lock()
+	defer sa.mu.Unlock()
+
+	if sa.filled > 0 {
+		sa.fillNeg(v)
+		return
+	}
+
+	sa.value *= float64(sa.period - 1)
+	sa.value -= v
+	sa.value /= float64(sa.period)
+}
+
+func (sa *SmoothedAverage) fillPos(v float64) {
+	sa.value += v
+	sa.fill()
+}
+
+func (sa *SmoothedAverage) fillNeg(v float64) {
+	sa.value -= v
+	sa.fill()
+}
+
+func (sa *SmoothedAverage) fill() {
+	sa.filled--
+
+	if sa.filled == 0 {
+		sa.value /= float64(sa.period)
+	}
+}
+
+func (sa *SmoothedAverage) Filled() bool {
+	sa.mu.Lock()
+	defer sa.mu.Unlock()
+
+	return sa.filled == 0
+}
+
+func (sa *SmoothedAverage) Value() float64 {
+	sa.mu.Lock()
+	defer sa.mu.Unlock()
+
+	if sa.filled > 0 {
+		return 0
+	}
+
+	return sa.value
+}
+
+func (sa *SmoothedAverage) Period() int {
+	sa.mu.Lock()
+	defer sa.mu.Unlock()
+
+	return sa.period
+}
