fb34dc58debe72fa3e17836db11148b9e1e04b2f
Yassel DG <yasseldg@gmail.com>
2024-06-01 10:03:37 +0000

    trading Symbol

--- trading/tSymbol/inter.go
+++ trading/tSymbol/inter.go
@@ -1,15 +1,33 @@
 package tSymbol
 
-import "github.com/yasseldg/go-simple/trading/tExchange"
+import (
+	"github.com/yasseldg/go-simple/trading/tExchange"
+
+	"github.com/yasseldg/mgm/v4"
+)
 
 type Inter interface {
-	Name() string
-	IsValid() bool
 	String() string
+	Log()
+
 	Exchange() tExchange.Inter
+
+	Name() string
 	Precision() int
-
 	SetPrecision(int)
-}
 
+	IsValid() bool
+	Clone() Inter
+	Model() InterModel
+}
 type Inters []Inter
+
+// InterModel
+
+type InterModel interface {
+	mgm.ModelDateState
+
+	ExchangeID() string
+	Name() string
+	Precision() int
+}
