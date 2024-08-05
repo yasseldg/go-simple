dc62e245406e03d2b26a18fd2d22c0c602345c96
Yassel DG <yasseldg@gmail.com>
2024-08-05 10:49:32 +0000

    types Json

--- types/sJson/jsons.go
+++ types/sJson/jsons.go
@@ -2,24 +2,32 @@ package sJson
 
 import (
 	"encoding/json"
-
-	"github.com/yasseldg/go-simple/logs/sLog"
 )
 
 func ToObj(msg string, obj any) error {
-	err := json.Unmarshal([]byte(msg), obj)
+	return ByteToObj([]byte(msg), obj)
+}
+
+func ByteToObj(data []byte, obj any) error {
+	err := json.Unmarshal(data, obj)
 	if err != nil {
-		sLog.Error("json.Unmarshal([]byte(msg), obj): %s", err)
 		return err
 	}
 	return nil
 }
 
 func ToString(v interface{}) (string, error) {
-	result, err := json.Marshal(v)
+	result, err := ToByte(v)
 	if err != nil {
-		sLog.Error("json.Marshal(v): %s", err)
 		return "", err
 	}
 	return string(result), nil
+}
+
+func ToByte(v interface{}) ([]byte, error) {
+	result, err := json.Marshal(v)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
 }
