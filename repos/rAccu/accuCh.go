45d40ebe45d5a349aa7250a1bed2c0555e20dcca
Yassel DG <yasseldg@gmail.com>
2024-10-01 18:38:32 +0000

    repos AccuCh

---
+++ repos/rAccu/accuCh.go
@@ -0,0 +1,29 @@
+package rAccu
+
+import (
+	"github.com/yasseldg/go-simple/repos/rMongo"
+)
+
+type AccuCh struct {
+	Inter
+
+	SaveCh chan struct{}
+}
+
+func NewAccuCH(coll rMongo.InterColl, limit int, saveCh chan struct{}) *AccuCh {
+	accu := AccuCh{
+		Inter:  New(coll, limit),
+		SaveCh: saveCh,
+	}
+
+	go accu.start()
+
+	return &accu
+}
+
+// save when saveCh is received
+func (a *AccuCh) start() {
+	for range a.SaveCh {
+		a.Save()
+	}
+}
