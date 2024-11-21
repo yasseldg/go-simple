d67eafb9abb2345de7f60f23d52625c5b0d2e439
Yassel DG <yasseldg@gmail.com>
2024-11-21 19:26:59 +0000

    trading Symbol

--- trading/tSymbol/funcs.go
+++ trading/tSymbol/funcs.go
@@ -6,10 +6,31 @@ import (
 	"github.com/yasseldg/go-simple/repos/rMongo"
 )
 
-func GetByExchangeNames(coll rMongo.InterRepo, exchange string, names ...string) (InterIterLimited, error) {
+type repo struct {
+	rMongo.InterRepo
+}
+
+func NewRepo(inter rMongo.InterRepo) InterRepo {
+	return &repo{
+		InterRepo: inter,
+	}
+}
+
+func (r *repo) GetByID(id rMongo.ObjectID) (Inter, error) {
+
+	var obj model
+	err := r.Clone().FindById(id, &obj)
+	if err != nil {
+		return nil, err
+	}
+
+	return &base{obj}, nil
+}
+
+func (r *repo) GetByExchangeNames(exchange string, names ...string) (InterIterLimited, error) {
 
 	var symbols []model
-	err := coll.Filters(NewFilters().Exchange(exchange).Name_In(names...)).
+	err := r.Clone().Filters(NewFilters().Exchange(exchange).Name_In(names...)).
 		Sorts(NewSorts().NameAsc()).Find(&symbols)
 	if err != nil {
 		return nil, fmt.Errorf("coll.Find(): %s", err)
